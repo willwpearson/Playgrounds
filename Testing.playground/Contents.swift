664e1d8002eb9b7bd65e5e0e66885fee261a326f
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-09-25 12:19:40 -0600

    Created code to try changing string values, to do string interpolation, counting character, and compare strings.

--- Testing.playground/Contents.swift
+++ Testing.playground/Contents.swift
@@ -2,4 +2,25 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// String change
+var someString = "xD"
+someString = "reeeeeeee"
+print(someString)
+
+// String interpolation
+let multiplier = 9
+let calculator = "\(multiplier) times 2.5 is \(Double(multiplier) * 2.5)"
+print(calculator)
+
+// Counting characters
+let hehe = "hehe xD"
+print("'hehe xD' has \(hehe.characters.count) characters")
+
+// Comparing strings
+let quote = "We are very similar ayyy"
+print(quote)
+let sameQuote = "We are very similar ayyy"
+print(sameQuote)
+if quote == sameQuote {
+    print("These two strings are considered equal")
+}
