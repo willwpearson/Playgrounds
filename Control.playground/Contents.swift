f52cb39ddfa0c9a448bf3d3cb8e2ad573fc87e5b
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-09-27 11:53:57 -0600

    Made two simple methods to understand how they work and how they are written.

---
+++ Control.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+public func isReallySimple( ) -> Void
+{
+    print ("xD lol")
+}
+isReallySimple()
+
+public func aBitLessSimple (name : String) -> Void
+{
+    let answer = "My name is not " + name
+    print(answer)
+}
+aBitLessSimple(name: "reeeeee")
+let words = "Jean"
+aBitLessSimple(name: words)
+
