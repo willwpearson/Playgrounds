c146039fa62340f44561792780cf3fa3f81231d9
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-10-02 12:50:22 -0600

    Demo with some sample classes and methods.

--- Classes.playground/Contents.swift
+++ Classes.playground/Contents.swift
@@ -2,7 +2,7 @@
 
 import UIKit
 
-var str = "Using Swift to make classes and methods"
+var notesToday = "Using Swift to make classes and methods"
 
 public class SimpleClass
 {
@@ -12,4 +12,44 @@ public class SimpleClass
     {
         name = "My name is ....."
     }
+    
+    public func getName() -> String
+    {
+        return name
+    }
 }
+var sample = SimpleClass()
+print(sample.getName())
+
+public class OtherClass
+{
+    private var favoriteNumber :Int
+    private var favoriteWord :String
+    
+    public init()
+    {
+        favoriteNumber = Int()
+        favoriteWord = String()
+    }
+    
+    public init(favoriteNumber :Int, favoriteWord :String)
+    {
+        self.favoriteNumber = favoriteNumber
+        self.favoriteWord = favoriteWord
+    }
+    
+    public func changeInternalState() -> Void
+    {
+        self.favoriteNumber *= 1238
+        self.favoriteWord += " and more and more"
+    }
+}
+//Swift Version
+var secondClass = OtherClass()
+//Java would be OtherClass sample = new OtherClass();
+//It is also not = OtherClass.init()
+var thirdSample = OtherClass(favoriteNumber: 876543, favoriteWord: "reeeee")
+secondClass.changeInternalState()
+thirdSample.changeInternalState()
+thirdSample.changeInternalState()
+
