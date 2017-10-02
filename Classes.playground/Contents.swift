d6d781cf769fcc3f54f9724743ea622b6cbf070f
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-10-02 12:24:46 -0600

    Added a sample class to the playground.

---
+++ Classes.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Using Swift to make classes and methods"
+
+public class SimpleClass
+{
+    private var name :String
+    
+    public init()
+    {
+        name = "My name is ....."
+    }
+}
