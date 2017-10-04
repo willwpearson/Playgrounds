9267934e88486216b560f2db80000a33650d8a5b
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-10-04 13:32:18 -0600

    Made two labels for the assignment.

--- GUI Playground.playground/Contents.swift
+++ GUI Playground.playground/Contents.swift
@@ -20,3 +20,14 @@ myFirstButton.backgroundColor = .green
 var mySecondButton = UIButton(frame: CGRect(x: 30, y: 60, width: 600, height: 120))
 mySecondButton.setTitle("xD lol", for: .normal)
 mySecondButton.backgroundColor = .red
+
+// Labels
+var firstLabel = UILabel(frame: CGRect(x: 30, y: 60, width: 100, height: 100))
+firstLabel.text = "lol"
+firstLabel.textColor = .red
+
+var secondLabel = UILabel(frame: CGRect(x: 30, y: 60, width: 100, height: 100))
+secondLabel.text = "xD"
+secondLabel.textColor = .blue
+
+
