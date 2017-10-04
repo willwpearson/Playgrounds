c8ad4f451f7e090ca88ab13a9355f7ff63c9923e
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-10-04 13:17:02 -0600

    Made UI Views rectangles.

--- GUI Playground.playground/Contents.swift
+++ GUI Playground.playground/Contents.swift
@@ -4,6 +4,15 @@ import UIKit
 
 var topic = "Working with GUI"
 
+// Views
+var firstView = UIView(frame: CGRect(x: 30, y: 60, width: 100, height: 100))
+firstView.backgroundColor = .blue
+var secondView = UIView(frame: CGRect(x: 30, y: 60, width: 100, height: 100))
+secondView.backgroundColor = .green
+var thirdView = UIView(frame: CGRect(x: 40, y: 70, width: 100, height: 100))
+thirdView.backgroundColor = .red
+
+// Buttons
 var myFirstButton = UIButton(frame: CGRect(x: 30, y: 60, width: 600, height: 120))
 myFirstButton.setTitle("I'm a button", for: .normal)
 myFirstButton.backgroundColor = .green
